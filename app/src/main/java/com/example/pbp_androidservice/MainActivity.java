4061b7644bc3b46be21ddbdb39d75809de469e78
MKMaarif <maarif2910@students.undip.ac.id>
2022-11-01 13:28:10 +0700

    test icon

--- app/src/main/java/com/example/pbp_androidservice/MainActivity.java
+++ app/src/main/java/com/example/pbp_androidservice/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.pbp_androidservice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    TextView noSongTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        noSongTextView = findViewById(R.id.tv_no_songs)
     }
+
 }
